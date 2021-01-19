f7b921ab4bf6814df6d1a2eb95ec286ac5fb0d77
Joel <joel.pantaleon@gmail.com>
2021-01-20 00:28:24 +0100

    First attempt at separating Rooms

---
+++ Room.py
@@ -0,0 +1,38 @@
+class Room:
+    def __init__(self, name, available_actions, usable_items):
+        self.name = name
+        self.available_actions = available_actions
+        self.usable_items = usable_items
+
+    def __repr__(self):
+        return self.name
+
+
+###ROOM SETUP GOES BELOW###
+# ("name" must be compatible with "the name" formula, [available actions], [usable items])
+# Moods are "okay", "great music", "terrible music"###
+smoking_room = Room(
+    "the smoking room",
+    ["[FLIRT]", "[BORROW] something", "[MOVE] to another room", "[LEAVE] the party"],
+    ["lollipop", "Soundcloud", "blunt", "cigarette", "chewing gum"],
+)
+bathroom = Room(
+    "the bathroom",
+    ["[FLIRT]", "drink [TAP] water", "[MOVE] to another room", "[LEAVE] the party"],
+    ["lollipop", "Soundcloud", "G", "poppers", "chewing gum"],
+)
+dance_floor = Room(
+    "the dance floor",
+    [
+        "[FLIRT]",
+        "[DANCE]",
+        "[BORROW] something",
+        "[MOVE] to another room",
+        "[LEAVE] the party",
+    ],
+    ["lollipop", "Soundcloud", "poppers", "chewing gum"],
+)
+
+every_room = [smoking_room, bathroom, dance_floor]
+# remember to add borrow to the actions available in rooms in the tutorial
+###ROOM SETUP ABOVE, Every new room must also be set up in Battle.move()###
--- TextBasedBattle.py
+++ TextBasedBattle.py
@@ -1,3 +1,5 @@
+from Scenes import *
+
 from Dialogue import (
     intro_text,
     intro_text2,
@@ -665,14 +667,14 @@ class NPC(Player):
         return "NPC: " + self.name
 
 
-class Room:
-    def __init__(self, name, available_actions, usable_items):
-        self.name = name
-        self.available_actions = available_actions
-        self.usable_items = usable_items
-
-    def __repr__(self):
-        return self.name
+# class Room:
+#    def __init__(self, name, available_actions, usable_items):
+#        self.name = name
+#        self.available_actions = available_actions
+#        self.usable_items = usable_items
+#
+#    def __repr__(self):
+#        return self.name
 
 
 class Party:
