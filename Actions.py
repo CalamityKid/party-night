e3f13b5e8cd3518685c6e61f5e731dc92bc56100
Joel <69771758+CalamityKid@users.noreply.github.com>
2021-05-23 02:43:59 +0200

    use_item barebones, loading items to g

--- Actions.py
+++ Actions.py
@@ -1,5 +1,6 @@
 from Blocks.Room import Room, rooms
-from Blocks.PlayerNPC import player, NPCs
+from Blocks.PlayerNPC import MainCharacter, NPC, player, NPCs
+from Blocks.Items import Item, items
 
 
 class Action:
@@ -23,7 +24,7 @@ class Action:
 class Move(Action):
     pass
 
-    def move(object_performing_the_action, object_to_be_interacted_with):
+    def execute(object_performing_the_action, object_to_be_interacted_with):
 
         if object_performing_the_action.location == object_to_be_interacted_with:
             print("You're already in this room.")
@@ -41,8 +42,92 @@ class Move(Action):
                 return None
 
 
-moveinstance = Move("move instance", "move")
-# Move.move(player, rooms["dance floor"]) THIS WORKS FINE
+class Use_Item(Action):
+    pass
 
+    def execute(object_performing_the_action, object_to_be_interacted_with):
+        if object_to_be_interacted_with not in object_performing_the_action.items:
+            print("You don't have that on you.")
+            return None
+        elif object_to_be_interacted_with in object_performing_the_action.items:
+            if (
+                object_to_be_interacted_with
+                in object_performing_the_action.active_items
+            ):
+                print("You've had that recently.")
+                return None
+            elif (
+                object_to_be_interacted_with
+                not in object_performing_the_action.active_items
+            ):
+                object_performing_the_action.items.remove(object_to_be_interacted_with)
+                print(object_to_be_interacted_with.use_text)
+                object_performing_the_action.active_items[
+                    object_to_be_interacted_with
+                ] = object_to_be_interacted_with.duration
+            # x = object_performing_the_action.items.pop(
+            # object_to_be_interacted_with.name)
+
+            # also remember to check if it has a delay
+
+
+#    def use_item(character, item_to_add):
+#        if item_to_add not in character.items:
+#            print("You don't have that on you.")
+
+#    def add_to_active_item(character, item_to_add):
+#        pass
+
+#   def use_item(
+#       self, item_used
+#  ):  ###Used to call battle.update items(), changed it to time sensitive
+#     if item_used not in self.items:
+#        print("You don't have that on you.")
+
+#           return None
+#       if item_used in self.items:
+#           self.active_items[item_used] = total_items[item_used]
+#           self.items.pop(item_used)
+#           if "Soundcloud" in item_used:
+#               self.Soundcloud_turn = 2
+#           if "G" in item_used:
+#               self.G_turn = 1
+#######print statements on what item is used
+print(player.items)
+player.items.append(items["lollipop"])
+print(player.items)
+Use_Item.execute(player, items["lollipop"])
+player.items.append(items["lollipop"])
+player.items.append(items["G"])
+Use_Item.execute(player, items["lollipop"])
+Use_Item.execute(player, items["G"])
+print(player.active_items)
+print(player.items)
+print(items)
+input()
+print("mouth is", player.mouth)
+print("cool is", player.coolness)
+player.update_active_items()
+print("mouth is", player.mouth)
+print("cool is", player.coolness)
+player.update_active_items()
+input()
+player.update_active_items()
+input()
+player.update_active_items()
+input()
+player.update_active_items()
+input()
+player.update_active_items()
+input()
+player.update_active_items()
+input()
+player.update_active_items()
+input()
+player.update_active_items()
+input()
+
+
+# Move.execute(player, rooms["dance floor"])  # THIS WORKS FINE
 # if isinstance(rooms["smoking room"], Room) == True:
 #    print("yus")
