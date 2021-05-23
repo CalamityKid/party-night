c6086e9cf55d287fa4211bb65c9419a7ea95c4bc
Joel <69771758+CalamityKid@users.noreply.github.com>
2021-05-23 20:27:42 +0200

    Character Input works fine

--- Format/formatting.py
+++ Format/formatting.py
@@ -1,11 +1,54 @@
-def format_input(to_format):
+def identify_object(object_to_be_identified_list, dict_of_objects):
+    if object_to_be_identified_list == None:
+        return None
+    for word in object_to_be_identified_list:
+        for key, objectx in dict_of_objects.items():
+            for identifierx in objectx.identifiers:
+                if identifierx in word:
+                    return objectx
+
+
+def identify_action(action_to_be_identified, dict_of_actions):
+    if action_to_be_identified == None:
+        return None
+    for key, objectx in dict_of_actions.items():
+        for identifierx in objectx.identifiers:
+            if identifierx in action_to_be_identified:
+                return objectx
+
+
+def clean_input(to_clean):
     result = None
-    if type(to_format) == str:
-        result = to_format.lower()
+    if type(to_clean) == str:
+        result = to_clean.lower()
         result = result.strip()
         return result
-    elif type(to_format) != str:
-        return print("format_input() wasn't fed a string.")
+    elif type(to_clean) != str:
+        print("clean_input wasn't fed a string.")
+        return None
+
+
+def format_input_command(player_input, dict_of_actions, dict_of_objects):
+    action = None
+    subject = None
+    player_input = clean_input(player_input)
+    player_input = player_input.split()
+    try:
+        action = player_input[0]
+        subject = player_input[1:]
+    except IndexError:
+        action = None
+        subject = None
+    #####################################################################
+    action = identify_action(action, dict_of_actions)
+    if action == None:
+        return None
+    #######################################################################
+    subject = identify_object(subject, dict_of_objects)
+    if subject == None:
+        return None
+    print(action, subject)
+    return action, subject
 
 
 # You feed it a list and it gives you a string. pretty sure i can make it take an attribute if I rewrite it with getattr
@@ -59,3 +102,17 @@ def format_objects_string(
             final_string += given_list[-2] + " and " + given_list[-1] + "."
 
         return final_string
+
+
+def inputyesorno():
+    yesornoresult = None
+    while yesornoresult == None:
+        yesornoresult = input()
+        if len(yesornoresult) > 0:
+            yesornoresult = yesornoresult.lower()
+            if "y" in yesornoresult[0]:
+                return True
+            elif "n" in yesornoresult[0]:
+                return False
+        print("(yes or no):", end=" ")
+        yesornoresult = None
