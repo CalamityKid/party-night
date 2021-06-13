de70a3ef5d710319160945c6472b97c4fbcf19f3
Joel <69771758+CalamityKid@users.noreply.github.com>
2021-06-13 22:02:57 +0200

    Solved Schedule problem

--- TODO.py
+++ TODO.py
@@ -3,4 +3,5 @@
 # work on the scenes
 # take some water "you cant use drink tap water"
 # remeber to add people to player.people_in_party in Scenes when they are introduced
-# figure out how to do update scedule wihtout a circular import
+# if music is bad -10 lit, if music is great +10 lit
+# if party is crowded takes time to move, if its not it doesn't.
