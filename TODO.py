f3042b1757ec20f2c7556dd808007f36e42d9b6c
Joel <69771758+CalamityKid@users.noreply.github.com>
2021-06-21 00:58:06 +0200

    coded all the couple scenes but they wont run

--- TODO.py
+++ TODO.py
@@ -1,7 +1,10 @@
 #####TO DO.
-# popper immediate use, takes time pass
-# work on the scenes
-# take some water "you cant use drink tap water"
+# Work on the scenes
+# Time passes when you use poppers, but does that make sense?
 # remeber to add people to player.people_in_party in Scenes when they are introduced
-# if music is bad -10 lit, if music is great +10 lit
-# if party is crowded takes time to move, if its not it doesn't.
+# "Changed Music" in memories. "Couple Convinced" in memories.
+# change the scedule to reflect couple times 3, que no cambien de lugar si no es couple times 4 at least
+# make your partner follow you around, he already is? maybe they should follow only sometimes?
+# can't make timed events work in Schedule cause of an import loop thing somehow
+# I think im drinking tap water on the dance floor for some reason
+# Couple Scenes dont run
