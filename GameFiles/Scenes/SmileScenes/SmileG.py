68824037895b83a7e5168d09e1b526d7c53c7e5d
Joel <69771758+CalamityKid@users.noreply.github.com>
2021-06-21 23:54:16 +0200

    Done with Russian Scenes

--- GameFiles/Scenes/SmileScenes/SmileG.py
+++ GameFiles/Scenes/SmileScenes/SmileG.py
@@ -1,111 +1,109 @@
-################################# Smile Borrow G
-def smileborrowGcode():
-    if "G" in player.active_items:
-        print(
-            """
-	Baby, we already did some. Chill, you don't wanna drop on us.
-	"""
-        )
+from ...Scripts.Blocks import items
+from ...Scripts.Actions import use_item
+from ...Input import yesorno
 
-    if "G" not in player.active_items:
-        if player.location != bathroom:
+################################# Smile Borrow G
+
+
+def smileGcontent(player):
+    print("")
+    if items["G"] in player.active_items:
+        print("          Baby, we already did some. Chill, you don't wanna drop on us.")
+        print("")
+        print("You DON'T wanna drop on them. Double dosing G is fucked up.")
+        print("You decide to wait it out.")
+        print("")
+        return None
+
+    elif items["G"] not in player.active_items:
+        if player.location != player.rooms["bathroom"]:
+            print("     Yeah, for sure! Let's have some, baby!")
+
+            if player.scenevariables.movementtimewarning == False:
+                print("")
+                print("      We'd have to go all the way to the bathroom, though.")
+                print("      I don't wanna do the whole trip by myself.")
+                print("      Especially when it's crowded. Takes forever. A nightmare.")
+                print("      We can still go though. If you want...")
+                print("")
+                player.scenevariables.movementtimewarning = True
             print(
-                """
-	Yeah, for sure! Let's have some, baby!
-	"""
-            )
-            if scenevariables.movementtimewarning == False:
-                print(
-                    """
-	We'd have to go all the way to the bathroom, though.
-    I don't wanna do the whole trip by myself.
-    Especially when it's crowded. Takes forever. A nightmare.
-    We can still go though. If you want...
-	"""
-                )
-                scenevariables.movementtimewarning = True
-            print(
-                "You sure you wanna go all the way to the bathroom for this? (y/n)",
+                "      You sure you wanna go all the way to the bathroom for this? (y/n)",
                 end=" ",
             )
             option = yesorno()
-            print("")
             if option == True:
-                player.location = bathroom
-                smile.location = bathroom
-                if party.crowded == "crowded":
+                player.location = player.rooms["bathroom"]
+                player.NPCs["smile"].location = player.rooms["bathroom"]
+                if player.party.crowd == "full":
+                    print("")
                     print(
-                        "The smile ambassador and you slowly bump your way the crowd around to the bathroom."
+                        "The smile ambassador and you slowly bump your way through the crowd."
                     )
-                    time.ten_minutes()
-                if party.crowded != "crowded":
+                    print("You finally make it to the bathroom.")
+                    print("")
+                    player.time.ten_minutes()
+                if player.party.crowd != "full":
+                    print("")
                     print("You both find yourselves in the bathroom in no time.")
+                    print("")
             if option == False:
-                print(
-                    """
-	Alright, if you wanna go do tell me though.
-	"""
-                )
+                print("")
+                print("          Alright, if you wanna go do tell me though!")
 
-        if player.location == bathroom:
+        if player.location == player.rooms["bathroom"]:
             if (
-                smile.times_talked == 3
+                player.NPCs["smile"].times_talked == 3
             ):  # This is if you havent met the pusher yet, similar to scene 3.
+                print("")
                 print(
-                    """
-	Here they are! Remember I told you I wanted to introduce you to my friend?
-
-Their friend is clearly a pusher. 
-They seem nice.
-You talk a bit, a lot of smiling on all parts."""
+                    "     Look who's here! Remember I told you I wanted to introduce you to my friend?"
                 )
-                time.ten_minutes()
-                people_in_party.append(pusher)
-                smile.times_talked = 4
+                print("The smile ambassador introduces you to their friend.")
+                print("Their friend is clearly a pusher.")
+                print("They seem nice.")
+                print("You end up talking a bit, there's smiling on both parts.")
+                print("You kinda end up not doing any G.")
+                print("")
+                player.people_in_party.append(player.NPCs["pusher"])
+                player.NPCs["pusher"].location = player.rooms["bathroom"]
+                player.NPCs["smile"].times_talked = 4
+                return True
 
-            if party.crowded == "crowded":
-                if scenevariables.stalltimewarning == False:
-                    print(
-                        """
-	It's pretty crowded in here.
-    We're gonna have to wait a bit to get a stall."""
-                    )
-                    input()
-                    print(
-                        """
-	It will probably take a while actually.
-	"""
-                    )
-                    scenevariables.stalltimewarning = True
-                print("You okay with waiting for a stall? (y/n)", end=" ")
+            if player.party.crowd == "full":
+                if player.scenevariables.stalltimewarning == False:
+                    print("")
+                    print("      It's pretty crowded in here.")
+                    print("      We're gonna have to wait a bit to get a stall.")
+                    print("")
+                    print("      It will probably take a while actually.")
+                    print("")
+                    player.scenevariables.stalltimewarning = True
+
+                print("      You okay with waiting for a stall? (y/n)", end="")
                 option = yesorno()
                 if option == True:
-                    print(
-                        """
-	Alright, let's do it.
+                    print("      Alright, let's do it.")
+                    print("")
+                    print("It IS pretty crowded. Waiting. Small talk.")
+                    print("After a few minutes a stall frees up.")
+                    print("You both get in.")
+                    print("")
+                    player.items.append(items["G"])
+                    use_item.execute(player, items["G"])
+                    return True
 
-It IS pretty crowded. Waiting. Small talk. 
-After a few minutes a stall frees up.
-You both get in."""
-                    )
-                    player.items["G"] = 40
-                    player.use_item("G")
-                    time.ten_minutes()
-                if option == False:
-                    print(
-                        """
-	Alright. Let me know if you change your mind."""
-                    )
+                elif option == False:
+                    print("      Alright. Let me know if you change your mind.")
+                    print("")
+                    return None
 
-            if party.crowded != "crowded":
-                print(
-                    """
-	Alright, let's do it!
-	
-and you go into an empty stall and do some G."""
-                )
-                player.items["G"] = 40
-                player.use_item("G")
-
-
-# smileborrowG = Scene("Smile Borrow G", smileborrowGcode)
+            if player.party.crowd != "full":
+                print("")
+                print("      Alright, let's do it!")
+                print("")
+                print("and you go into an empty stall together")
+                print("")
+                player.items.append(items["G"])
+                use_item.execute(player, items["G"])
+                return True
--- TODO.py
+++ TODO.py
@@ -1,10 +1,11 @@
 #####TO DO.
-# Work on the scenes
-# Time passes when you use poppers, but does that make sense?
 # remeber to add people to player.people_in_party in Scenes when they are introduced
-# "Changed Music" in memories. "Couple Convinced" in memories.
-# change the scedule to reflect couple times 3, que no cambien de lugar si no es couple times 4 at least
-# make your partner follow you around, he already is? maybe they should follow only sometimes?
-# can't make timed events work in Schedule cause of an import loop thing somehow
+# "Changed Music" in memories(couple/maybe partner?). "Couple Convinced" in memories.
+# "Attending Film Festival" in memories (Russian), or "Not Attending Film Festival".
+# Change people in room narration so it doesnt state your partner in it in the list.
 # I think im drinking tap water on the dance floor for some reason
-# Couple Scenes dont run
+# Narrate everything before each turn, make text more interactive, maybe make it a setting.
+# Make an ask for action and put the asking items in there.
+# aslo Smile use G has too many spaces, add narration to quotes
+# Partner gives you "Borrow" in memories
+# make a coke item, used in divinity (Russian 5)
