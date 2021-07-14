f995139bfc9591b4463a1d35d8055c908539a4bf
Joel <69771758+CalamityKid@users.noreply.github.com>
2021-07-14 19:13:31 +0200

    fixed some minor stuff

--- GameFiles/Scripts/Format/DanceNarration.py
+++ GameFiles/Scripts/Format/DanceNarration.py
@@ -1,58 +1,66 @@
-modified_cause_dict = {"lit": "mod_lit", "high": "mod_high", "cool": "mod_cool"}
+modified_cause_dict = {"lit": "mod_lit", "high": "mod_high", "coolness": "mod_coolness"}
+
 
 def narrate_result(player, result):
     dance_flourish()
-    if result <= 40: #bad result
-        print ("Bad result this time")
+    if result <= 40:  # bad result
+        print("Bad result this time")
 
         pass
-    
-    elif result > 40 and result <70: #ok result
+
+    elif result > 40 and result < 70:  # ok result
         pass
 
-    elif result >= 70 and result <100: #great results
+    elif result >= 70 and result < 100:  # great results
         pass
 
-    elif result >= 100: 
+    elif result >= 100:
         pass
 
 
 def dance_flourish():
-    print ("****")
-    print (" **")
-    print ("   *")
+    print("****")
+    print(" **")
+    print("   *")
     return None
 
 
 def identify_causes(player, good_or_bad):
     causes = []
-    if good_or_bad == True:
+    if good_or_bad == False:
         for attr in ["lit", "high"]:
-            if getattr(player,attr) <= 40:
+            if getattr(player, attr) <= 30:
                 causes.append(attr)
-        if player.coolness => 50:
+        if player.coolness >= 60:
             causes.append("coolness")
-            return causes
-    elif good_or_bad == False:
+
+    elif good_or_bad == True:
         for attr in ["lit", "high"]:
-            if getattr(player,attr) >= 40:
+            if getattr(player, attr) >= 50:
                 causes.append(attr)
-        if player.coolness <= 50:
+        if player.coolness <= 30:
             causes.append("coolness")
-            return causes
+    print(good_or_bad, "causes are", causes)
+    return causes
 
 
-#checks if theres a 30 point dif between a player stat and a modded stat
-def mod_helped (player, dance_instance, good_or_bad): 
-    result = {}
-    if good_or_bad == True:
-        for key, value in modified_cause_dict.items():
-            if dance_instance.value - player.key > 30:
-                result[key] = True
-    
-    elif good_or_bad == False:
-        for key, value in modified_cause_dict.items():
-            if player.key - dance_instance.value > 30:
-                result[key] = True
-    
+# checks if theres a 30 point dif between a player stat and a modded stat
+def mod_helped(player, dance_instance, good_or_bad):
+    result = []
+    print("mod:", good_or_bad)
+    for key, value in modified_cause_dict.items():
+        instance_value = getattr(dance_instance, value)
+        player_value = getattr(player, key)
+        print(key, value)
+        print(player_value, instance_value)
+
+        if good_or_bad == True:
+            if instance_value - player_value > 30:
+                result.append(key)
+
+        elif good_or_bad == False:
+            if player_value - instance_value > 30:
+                result.append(key)
+
+    print("mod results are", result)
     return result
