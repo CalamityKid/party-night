d7faaf344dbae2bd574a926aa0fa05972c4ae229
Joel <69771758+CalamityKid@users.noreply.github.com>
2021-10-26 23:30:29 +0200

    fixed a lot of things but fucked up player stats

--- TODO.py
+++ TODO.py
@@ -8,6 +8,16 @@
 # # aslo Smile use G has too many spaces.
 # write and program the flirt function and the calculations
 # Write partner, pusher ("you get a comission") + gameover for each
+# partner scene when you convince them to come down, times tlaked4, they go back for some reason
+# smile doesnt offer G when in bathroom in times tlaked 1
 
 # MAYBE
 # Narrate everything before each turn, make text more interactive, maybe make it a setting.
+
+
+# in dance
+##if successful dance, trigger dance scenes for people in room, if possible
+# really confirm that the outcome_on_stats works fine but it prob does
+# consider making a formatting function that edits the length of sentences before theyre printed.
+# the cool conjunction is weird
+# the stats are all fucked up but I modified a lot of player stats so we gotta check that as well
