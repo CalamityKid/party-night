51b685658e4f75738d254813f6d899fdaab1be44
Joel <69771758+CalamityKid@users.noreply.github.com>
2022-07-26 11:07:54 +0200

    Flirt Bark works

--- GameFiles/Scripts/Format/Bark.py
+++ GameFiles/Scripts/Format/Bark.py
@@ -1,71 +1,84 @@
-from random import randint
+from random import randint, choice
+from ...Scenes.Flirt.GoodBarkText import (
+    no_item_string,
+    good_bark_item_string_dict,
+    body_check_bark_dict,
+    good_bark_room_string_dict,
+    good_bark_stats_string_dict,
+    ending_string_dict,
+)
+from ...Scenes.Flirt.BadBarkText import (
+    bad_no_item_string,
+    bad_bark_item_string_dict,
+    bad_bark_room_string_dict,
+    bad_bark_stats_string_dict,
+    bad_ending_string_dict,
+)
 
-###################### BODY CHECK BARK
-body_check_bark_dict = {
-    0: "This is what's up now:",
-    1: "This is what's popping:",
-    2: "The lay of the land:",
-    3: "Latest updates:",
-    4: "Current party situation:",
-    5: "STATUS:",
-    6: "You connect to your inner being:",
-    7: "You listen to your body:",
-    8: "SITUATION REPORT:",
-    9: "What had happened was:",
-}
+# The text is in the BarkText files in the Flirt Scenes
 
 
 def body_check_bark():  ##Random opening statement before body check
+    """prints a randomized opening statement before a body check"""
     print(" ")
     op = randint(0, 9)
     print(body_check_bark_dict[op])
     return None
 
 
-####################### FLIRT BARK
-
-
-def flirt_bark_good(player):
-    """Pass the player to it and though some randomizing and player stats it'll return a string saying that the flirt went well"""
+### GOOD BARK
+def flirt_bark_good(player_obj):
+    """Pass the player to it and though some randomizing and current stats it'll return a string saying that the flirt went well"""
     final_string_list = []
     final_string = ""
+    randselect = randint(0, 2)
+
+    if randselect == 0:  # active item select
+        if len(player_obj.active_items) == 0:
+            final_string_list.append(no_item_string)
+
+        elif len(player_obj.active_items) > 0:
+            randitem = choice(list(player_obj.active_items.keys()))
+            final_string_list.append(good_bark_item_string_dict[randitem])
+
+    elif randselect == 1:  # room
+        final_string_list.append(good_bark_room_string_dict[player_obj.location.name])
+
+    elif randselect == 2:  # stats
+        randstat = randint(0, 2)
+        final_string_list.append(good_bark_stats_string_dict[randstat])
+
+    # second sentence
+    randending = randint(0, 4)
+    final_string_list.append(ending_string_dict[randending])
+    final_string = "".join(final_string_list)
+    return final_string
 
 
-""" "title": "FlirtGoodBark(Person)",
-		"tags": "flirt good bark",
-		"body": ">><< random   0-2>>
-<< if 0>> #active item
-    <<if length of active items is 0>> "being clearheaded seems to help because"
-    <<if length of active items is 1, select one>>
-    <<if cig>> "maybe it's the nicotine but"
-    <<if blunt>> "pure ganja suaveness,"
-    <<if soundcloud>> "you're feeling joyful and it shows,"
-    <<if coke>> "you get the twitch under control and"
-    <<if specialK>> "the good vibes are felt,"
-    <<if gum>> "minty fresh magic,"
-    <<if lolipop>> "the lollipop complements the compliments,"
-    <<if g>> "you're feeling relaxed and"
-    <<if popp>> "you're still feeling a lightheaded but"
+###BAD BARK
+def flirt_bark_bad(player_obj):
+    """Pass the player to it and though some randomizing and current stats it'll return a string saying that the flirt went badly"""
+    final_string_list = []
+    final_string = ""
+    randselect = randint(0, 2)
 
+    if randselect == 0:  # active item select
+        if len(player_obj.active_items) == 0:
+            final_string_list.append(bad_no_item_string)
 
-<< if 1>> #environment
-    <<if player.location == bathroom>> "bathroom flirting,"
-    <<if player.location == dance floor>> "the vibe of the dance floor helps,"
-    <<if player.location == smoking room>> "it's quiet in here, it helps and"
+        elif len(player_obj.active_items) > 0:
+            randitem = choice(list(player_obj.active_items.keys()))
+            final_string_list.append(bad_bark_item_string_dict[randitem])
 
-<< if 2>> #stats
-    <<random 0-2>>
-    <<if 0>> "you're feeling cool,"
-    <<if 1>> "the party feels more fun,"
-    <<if 2>> "you start riding the wave and"
+    elif randselect == 1:  # room
+        final_string_list.append(bad_bark_room_string_dict[player_obj.location.name])
 
-#second part
-<<random 0-4>>
- <<if 0>> add " it goes well, you both smile."
- <<if 1>> add " you really nail it. Sparks fly."
- <<if 2>> add " you get the cutest smile in return."
- <<if 3>> add " you end up sounding charming as hell."
- <<if 4>> add " there's really good chemistry between you."
+    elif randselect == 2:  # stats
+        randstat = randint(0, 2)
+        final_string_list.append(bad_bark_stats_string_dict[randstat])
 
-return 1 + 2
-"""
+    # second sentence
+    randending = randint(0, 4)
+    final_string_list.append(bad_ending_string_dict[randending])
+    final_string = "".join(final_string_list)
+    return final_string
