d6aadb5d658bf0e2becb2cd54379efbb80bad988
Joel <69771758+CalamityKid@users.noreply.github.com>
2022-10-12 13:49:46 +0200

    Think only one more scene?

--- TODO.py
+++ TODO.py
@@ -1,7 +1,6 @@
 #####REMEMBER.
 # remeber to add people to player.people_in_party in Scenes when they are introduced
 # fix borrow to only include the people in the room!
-# Attending Film Festival and Not Attending Film Festival to russian game over
 # add so many spaces everywhere
 # mae sure the dance scenes run properly, also make scenes for pusher or so
 # write gameover screens
@@ -13,9 +12,6 @@
 # # aslo Smile use G has too many spaces.
 # consider making a save function.
 
-# MAYBE
-# Narrate everything before each turn, make text more interactive, maybe make it a setting.
-
 # in dance
 # the stats are all fucked up but I modified a lot of player stats so we gotta check that as well
 
@@ -23,10 +19,3 @@
 
 # body update or something is out of control
 # theres a cool loop also, it cannot go down at all, its forever high, maybe bad dance should really get it down
-
-
-def partnertimes1content(player):
-    sleep(2)
-    player.modify_stat("lit", 10, True)
-    player.NPCs["partner"].times_talked = 2
-    return True
