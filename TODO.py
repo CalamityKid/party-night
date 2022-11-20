05b63b38381f435e47bf834eb48878d3a14d10b7
Joel <69771758+CalamityKid@users.noreply.github.com>
2022-11-20 07:44:36 +0100

    Spaces to russian

--- TODO.py
+++ TODO.py
@@ -1,7 +1,7 @@
 #####REMEMBER.
 # remeber to add people to player.people_in_party in Scenes when they are introduced
-# add so many spaces everywhere. bad music only fucs you up on the dance floor. is smile in the bathroom after 2 for long enough?
-# add music to dance. If on dance floor narrate music. Add time. Remove 30 minute refresh thing.
+# add so many spaces everywhere. bad music only fucs you up on the dance floor.
+# add music to dance. If on dance floor narrate music.
 # Get the flow down properly in partner and the gather scenes, wtf is cool going up so much
 # Turn off debug in formatting.format_input_command when compiling. LEAVE party. ok in select option.
 
@@ -11,11 +11,11 @@
 
 # in dance
 # gameoversmile "pusher friend met?" even though we hadnt i think
-# add spaces to Rss, and Cutie
+# add spaces to Cutie
 # fix anxiety thingsss (in player probably)
 # in narrate talk, chec if theres more than 4 people do it on separate lines
 # gameover action narration "you get out of the club"?
-# consider maing meeting cutie and pusher easier?
+# consider maing meeting cutie easier?
 # the stats are all fucked up but I modified a lot of player stats so we gotta check that as well
 
 # check if the Tanktop dance thing, flirt > 5 is ok or not, amarrar la scene partner tantop5
