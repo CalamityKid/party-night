dab6eec48528cb4e235a24f2d7d369dffa2b6b11
Joel <69771758+CalamityKid@users.noreply.github.com>
2022-12-11 07:58:45 +0100

    some fixes

--- TODO.py
+++ TODO.py
@@ -1,22 +1,16 @@
 #####REMEMBER.
 # remeber to add people to player.people_in_party in Scenes when they are introduced
-# add so many spaces everywhere. bad music only fucs you up on the dance floor.
+# add so many spaces everywhere.
 # add music to dance. If on dance floor narrate music.
-# Get the flow down properly in partner and the gather scenes, wtf is cool going up so much
-# Turn off debug in formatting.format_input_command when compiling. LEAVE party. ok in select option.
+# Get the flow down properly in partner and the gather scenes
+#  LEAVE party. ok in select option.
 
 # TO DO
-# just deleted 30 m update, see if that checks out.
 # consider making a save function.
 
 # in dance
 # gameoversmile "pusher friend met?" even though we hadnt i think. Partner "it was a fun plan but a bit silly"
-# fix anxiety thingsss (in player probably). PARTNER spaces. Use G tons of spaces ad no where they should be
+# Use G tons of spaces ad no where they should be
 # in narrate talk, chec if theres more than 4 people do it on separate lines (">3?") works
-# gameover action narration "you get out of the club"?.pusher gives you K item.
-# the stats are all fucked up but I modified a lot of player stats so we gotta check that as well
 
 # check if the Tanktop dance thing, flirt > 5 is ok or not, amarrar la scene partner tantop5
-
-# body update or something is out of control
-# theres a cool loop also, it cannot go down at all, its forever high, maybe bad dance should really get it down
