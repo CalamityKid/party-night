4ee68d9bb58dd6968727e12ad46f13963446980f
Joel <69771758+CalamityKid@users.noreply.github.com>
2022-12-15 01:20:28 +0100

    cleaned setup and main cant restart game properly

--- TODO.py
+++ TODO.py
@@ -3,7 +3,6 @@
 # add so many spaces everywhere.
 # add music to dance. If on dance floor narrate music.
 # Get the flow down properly in partner and the gather scenes
-# ok in select option.
 
 # TO DO
 # consider making a save function.
@@ -11,6 +10,5 @@
 # in dance
 # gameoversmile "pusher friend met?" even though we hadnt i think. Partner "it was a fun plan but a bit silly"
 # Use G tons of spaces ad no where they should be
-# in narrate talk, chec if theres more than 4 people do it on separate lines (">3?") works
 
 # check if the Tanktop dance thing, flirt > 5 is ok or not, amarrar la scene partner tantop5
